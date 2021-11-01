435e423d63d85c6b9a3513ff32107ca46dd738f2
jianwang10 <jianwang10@iflytek.com>
2021-11-01 20:54:43 +0800

    归并排序

--- leetcode/src/com/ds/sort/MergeSort.java
+++ leetcode/src/com/ds/sort/MergeSort.java
@@ -1,4 +1,37 @@
 package sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {1,3,8,6,5,18,13,2};
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if(left >= right) return;
+        int mid = left + ((right - left) >> 1);
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] tmp = new int[right - left + 1];
+        int i = left,j = mid + 1, k = 0;
+        while (i <= mid && j <= right){
+            tmp[k++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+        }
+        while (i <= mid){
+            tmp[k++] = arr[i++];
+        }
+        while(j <= right){
+            tmp[k++] = arr[j++];
+        }
+        for(int h =0; h < tmp.length; h++){
+           arr[left + h] = tmp[h];
+        }
+    }
 }
