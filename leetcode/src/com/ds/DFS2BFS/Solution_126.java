6c6fd3b1be66368891156817eea2a006ad923ba1
jianwang10 <jianwang10@iflytek.com>
2021-11-07 21:28:41 +0800

    单词接龙126

---
+++ leetcode/src/com/ds/DFS2BFS/Solution_126.java
@@ -0,0 +1,153 @@
+package DFS2BFS;//按字典 wordList 完成从单词 beginWord 到单词 endWord 转化，一个表示此过程的 转换序列 是形式上像 beginWord -> s
+//1 -> s2 -> ... -> sk 这样的单词序列，并满足： 
+//
+// 
+// 
+// 
+// 每对相邻的单词之间仅有单个字母不同。 
+// 转换过程中的每个单词 si（1 <= i <= k）必须是字典 wordList 中的单词。注意，beginWord 不必是字典 wordList 中的单
+//词。 
+// sk == endWord 
+// 
+//
+// 给你两个单词 beginWord 和 endWord ，以及一个字典 wordList 。请你找出并返回所有从 beginWord 到 endWord 的
+// 最短转换序列 ，如果不存在这样的转换序列，返回一个空列表。每个序列都应该以单词列表 [beginWord, s1, s2, ..., sk] 的形式返回。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","lo
+//g","cog"]
+//输出：[["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+//解释：存在 2 种最短的转换序列：
+//"hit" -> "hot" -> "dot" -> "dog" -> "cog"
+//"hit" -> "hot" -> "lot" -> "log" -> "cog"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","lo
+//g"]
+//输出：[]
+//解释：endWord "cog" 不在字典 wordList 中，所以不存在符合要求的转换序列。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= beginWord.length <= 7 
+// endWord.length == beginWord.length 
+// 1 <= wordList.length <= 5000 
+// wordList[i].length == beginWord.length 
+// beginWord、endWord 和 wordList[i] 由小写英文字母组成 
+// beginWord != endWord 
+// wordList 中的所有单词 互不相同 
+// 
+// 
+// 
+// Related Topics 广度优先搜索 哈希表 字符串 回溯 
+// ? 505 ? 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+import java.util.*;
+
+public class Solution_126 {
+    public static void main(String[] args) {
+        List<List<String>> res = findLadders("hit", "cog", Arrays.asList("hot","dot","dog","lot","log","cog"));
+        System.out.println(res);
+    }
+
+    public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> res = new ArrayList<>();
+        // 因为需要快速判断扩展出的单词是否在 wordList 里，因此需要将 wordList 存入哈希表，这里命名为「字典」
+        Set<String> dict = new HashSet<>(wordList);
+        // 特殊用例判断
+        if (!dict.contains(endWord)) {
+            return res;
+        }
+        // 因为从 beginWord 开始扩展，因此 dict 里一定不可以有 beginWord
+        dict.remove(beginWord);
+        // 第 1 步：广度优先遍历构建图
+        // 为了避免记录不需要的边，我们需要记录扩展出的单词是在第几次扩展的时候得到的，key：单词，value：在广度优先遍历的第几层
+        // steps 记录了已经访问过的 word 集合，同时记录了在第几层访问到
+        Map<String, Integer> steps = new HashMap<>();
+        steps.put(beginWord, 0);
+        // 记录了单词是从哪些单词扩展而来，key：单词，value：单词列表，这些单词可以变换到 key ，它们是一对多关系，dfs 的时候会用到
+        Map<String, Set<String>> from = new HashMap<>();
+        boolean found = bfs(beginWord, endWord, dict, steps, from);
+        // 第 2 步：深度优先遍历找到所有解，从 endWord 恢复到 beginWord ，所以每次尝试操作 path 列表的头部
+        if (found) {
+            Deque<String> path = new ArrayDeque<>();
+            path.add(endWord);
+            dfs(from, path, beginWord, endWord, res);
+        }
+        return res;
+    }
+
+
+    private static boolean bfs(String beginWord, String endWord, Set<String> dict, Map<String, Integer> steps, Map<String, Set<String>> from) {
+        int wordLen = beginWord.length();
+        int step = 0;
+        boolean found = false;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            step++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String currWord = queue.poll();
+                char[] charArray = currWord.toCharArray();
+                for (int j = 0; j < wordLen; j++) {
+                    char origin = charArray[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        // 将每一位替换成 26 个小写英文字母
+                        charArray[j] = c;
+                        String nextWord = String.valueOf(charArray);
+                        // 注意：这几行代码的逻辑先后顺序
+                        if (steps.containsKey(nextWord) && steps.get(nextWord) == step) {
+                            from.get(nextWord).add(currWord);
+                        }
+                        if (!dict.contains(nextWord)) {
+                            continue;
+                        }
+                        dict.remove(nextWord);// dict 和 steps 承担了已经访问的功能
+                        queue.offer(nextWord);
+                        // 维护 from、steps、found 的定义
+                        from.putIfAbsent(nextWord, new HashSet<>());
+                        from.get(nextWord).add(currWord);
+                        steps.put(nextWord, step);
+                        if (nextWord.equals(endWord)) {
+                            // 注意：由于有多条路径到达 endWord 找到以后不能立即退出，只需要设置 found = true 即可
+                            found = true;
+                        }
+                    }
+                    charArray[j] = origin;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+        return found;
+    }
+
+    private static void dfs(Map<String, Set<String>> from, Deque<String> path, String beginWord, String cur, List<List<String>> res) {
+        if (cur.equals(beginWord)) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (String precursor : from.get(cur)) {
+            path.addFirst(precursor);
+            dfs(from, path, beginWord, precursor, res);
+            path.removeFirst();
+        }
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
