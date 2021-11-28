2d0c0575005f189ff9149aa695acac01161284fa
jianwang10 <jianwang10@iflytek.com>
2021-11-28 12:53:53 +0800

    轮转数组

---
+++ leetcode/src/com/ds/array/Solution_189.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+class Solution_189 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        rotate(arr, 3);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+//            start += 1;
+//            end -= 1;
+        }
+    }
+}
