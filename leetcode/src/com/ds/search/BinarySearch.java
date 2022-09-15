17ba43c557bd7417213d984a17a5952be33a3e1f
jianwang10 <jianwang10@iflytek.com>
2022-09-15 14:59:59 +0800

    search

--- leetcode/src/com/ds/search/BinarySearch.java
+++ leetcode/src/com/ds/search/BinarySearch.java
@@ -9,5 +9,22 @@ public class BinarySearch {
 
 
 	}
+	public static int binarySearch(int[] arr, int left, int right, int findVal) {
+		System.out.println("二分查找被调用~");
+		// 当 left > right 时，说明递归整个数组，但是没有找到
+		if (left > right) {
+			return -1;
+		}
+		int mid = (left + right) / 2;
+		int midVal = arr[mid];
 
+		if (findVal > midVal) { // 向 右递归
+			return binarySearch(arr, mid + 1, right, findVal);
+		} else if (findVal < midVal) { // 向左递归
+			return binarySearch(arr, left, mid - 1, findVal);
+		} else {
+
+			return mid;
+		}
+	}
 }
